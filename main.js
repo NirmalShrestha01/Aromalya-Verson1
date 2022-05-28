03e687d0e935f56776015a9419d36c86a09eb40c
Nirmal <nirmal.work01@gmail.com>
2022-05-28 15:04:05 +0545

    after minor correction verson 3

--- main.js
+++ main.js
@@ -19,3 +19,39 @@
   } 
 
 // STICKY NAVBAR SECTION END
+
+
+
+// PAYMENT DETAILS JS START
+
+
+var coll = document.getElementsByClassName("collapsible_cards");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var card_details = this.nextElementSibling;
+    if (card_details.style.display === "block") {
+      card_details.style.display = "none";
+    } else {
+      card_details.style.display = "block";
+    }
+  });
+}
+var coll = document.getElementsByClassName("collapsible_paypal");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var paypal_details = this.nextElementSibling;
+    if (paypal_details.style.display === "block") {
+      paypal_details.style.display = "none";
+    } else {
+      paypal_details.style.display = "block";
+    }
+  });
+}
+
+// PAYMENT DETAILS JS END
